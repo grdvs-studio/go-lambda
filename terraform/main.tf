94a7aca79d22f3ad834a3b406c9376af32e942ae
Dmitry Grinko <dm.grinko@gmail.com>
2026-01-19 14:57:11 -0800

    test6

--- terraform/main.tf
+++ terraform/main.tf
@@ -58,6 +58,7 @@ resource "aws_lambda_function" "health_check" {
   handler          = "bootstrap"
   source_code_hash = filebase64sha256(var.lambda_zip_path)
   runtime          = "provided.al2023"
+  architectures    = ["arm64"]
 
   timeout     = 30
   memory_size = 128
